b9ee8dcedc371c17387134e90d434b775e396504
daichi3102 <k.daichi.p3@gmail.com>
2024-05-03 19:04:52 +0900

    i18nの日本語化対応

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,10 +11,13 @@ module Wordpass
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    # Set the default locale to Japanese
+    config.i18n.default_locale = :ja
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
