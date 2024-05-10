20533960dac5be2cfc70c29329bc52a5703037ad
daichi3102 <k.daichi.p3@gmail.com>
2024-05-10 11:57:32 +0900

    編集し忘れ

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory = 'バケット名' # バケット名
+  config.fog_directory = 'raccobucket' # バケット名
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
