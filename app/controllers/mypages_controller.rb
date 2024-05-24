9569cbe21d3aac039484a7a71530f507a9fdc605
daichi3102 <k.daichi.p3@gmail.com>
2024-05-24 22:11:40 +0900

    ソート順の修正

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -3,8 +3,8 @@ class MypagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @likes = current_user.likes
-    @makes = current_user.makes
-    @fetch_ais = current_user.fetch_ais
+    @likes = current_user.likes.includes(:make).order(created_at: :desc).map(&:make)
+    @makes = current_user.makes.order(created_at: :desc)
+    @fetch_ais = current_user.fetch_ais.order(created_at: :desc)
   end
 end
