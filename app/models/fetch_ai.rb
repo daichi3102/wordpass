5a2dc0e08c1b7a8578fe57f7997cfa362649a4e1
daichi3102 <k.daichi.p3@gmail.com>
2024-06-12 14:37:04 +0900

    検索機能の追加

--- app/models/fetch_ai.rb
+++ app/models/fetch_ai.rb
@@ -7,7 +7,7 @@ class FetchAi < ApplicationRecord
   belongs_to :user, optional: true
 
   # Ransackで使用するスコープを追加
-  def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "how", "id", "mood", "popularity", "quote_type", "response", "schedule", "updated_at", "user_id"]
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at how id mood popularity quote_type response schedule updated_at user_id]
   end
 end
