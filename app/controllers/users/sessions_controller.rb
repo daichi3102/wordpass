9b8950fae893817fc8eafab58bcacd2302902274
daichi3102 <k.daichi.p3@gmail.com>
2024-06-28 15:37:46 +0900

    テスト結果の修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
-class Users::SessionsController < Devise::SessionsController
-  before_action :configure_sign_in_params, only: [:create]
+module Users
+  class SessionsController < Devise::SessionsController
+    before_action :configure_sign_in_params, only: [:create]
 
-  def create
-    super
-  end
+    protected
 
-  protected
-
-  def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+    def configure_sign_in_params
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+    end
   end
 end
